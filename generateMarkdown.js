b936dc5b8d9ed86d1b7b6b850d1b12b97d443413
Mark Green Jr <mrgssg@gmail.com>
2023-01-31 18:47:48 -0500

    started adding functions to index.js and generate.js files

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,10 +1,21 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let license = "";
+  
+  if (!license) {
+    return "no license entered";
+  }
+
+  return license;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink;
+  return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
--- index.js
+++ index.js
@@ -6,42 +6,42 @@ const questions = [
         type: "input",
         name: "title",
         message: "enter the project title:"
-    }
+    },
     {
         type: "input",
         name: "description",
         message: "enter a description:"
-    }
+    },
     {
         type: "input",
         name: "Table of Contents",
         message: "enter the table of contents:"
-    }
+    },
     {
         type: "input",
         name: "install",
         message: "enter installation instructions:"
-    }
+    },
     {
         type: "input",
         name: "usage",
         message: "enter the information on how to use this readme:"
-    }
+    },
     {
         type: "input",
         name: "license:",
         message: "enter project license:"
-    }
+    },
     {
         type: "input",
         name: "contributing guidelines",
         message: "enter contribution guidelines:"
-    }
+    },
     {
         type: "input",
         name: "test instructions",
         message: "enter test instructions:"
-    }
+    },
     {
         type: "input",
         name: "questions",
@@ -51,7 +51,7 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    
+    const fileName = "./README.md";
 }
 
 // TODO: Create a function to initialize app
