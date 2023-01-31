5f503e100b00acf13aeecb86e5f77dcf2feabdce
Mark Green Jr <mrgssg@gmail.com>
2023-01-30 19:18:50 -0500

    added array of questions to index.js file

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "enter the project title:"
+    }
+    {
+        type: "input",
+        name: "description",
+        message: "enter a description:"
+    }
+    {
+        type: "input",
+        name: "Table of Contents",
+        message: "enter the table of contents:"
+    }
+    {
+        type: "input",
+        name: "install",
+        message: "enter installation instructions:"
+    }
+    {
+        type: "input",
+        name: "usage",
+        message: "enter the information on how to use this readme:"
+    }
+    {
+        type: "input",
+        name: "license:",
+        message: "enter project license:"
+    }
+    {
+        type: "input",
+        name: "contributing guidelines",
+        message: "enter contribution guidelines:"
+    }
+    {
+        type: "input",
+        name: "test instructions",
+        message: "enter test instructions:"
+    }
+    {
+        type: "input",
+        name: "questions",
+        message: "enter any questions:"
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
